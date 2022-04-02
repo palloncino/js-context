a5dd9a0f0d031a59113c7b083b14866b3d35fe33
palloncino <antonio.guiotto.dev@gmail.com>
2022-04-02 12:20:49 +0200

    startign point

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { useEffect } from 'react';
 import { ctx } from './context';
+import { useInitialData } from './useInitialData';
 
 function App() {
+
+  const data = useInitialData();
+
+  useEffect(() => console.log(data), [data]);
+
   return (
     <div className="App">
-      <ctx.Provider>
-
+      <ctx.Provider value={data}>
+        <div></div>
       </ctx.Provider>
     </div>
   );
